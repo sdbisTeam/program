6e56ee155a2812f54007ef78c228078c10f70b92
Home <Home@DESKTOP-O3Q9PMN>
2019-03-04 12:10:07 +0200

    commit 1

---
+++ Program/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+		Name n = new Name();  
+		String str = "";		
+		String str2 = new String();
+		str2 =n.console(str);
+	        
+	         File file = new File("C:\\Users\\Home\\Desktop\\penta\\name.txt");
+	         if (!file.exists()) {
+	            file.createNewFile();
+	         } 
+	         FileWriter fw = new FileWriter(file.getAbsoluteFile());
+	         BufferedWriter bw = new BufferedWriter(fw);
+	         bw.write(str2);
+	         bw.close();
+	         
+	         System.out.println("Done");
+	      } catch (IOException e) {
+	         e.printStackTrace();
+	      } 
+		
+		
+	}
+
+}
---
+++ Program/src/Name.java
@@ -0,0 +1,36 @@
+
+import java.util.Scanner;
+
+public class Name {
+
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Name(String name) {
+		super();
+		this.name = name;
+	}
+
+	public Name() {
+		super();
+	}
+	
+	public String console(String str) {
+		
+		
+		System.out.println("What is your name?");
+		Scanner name = new Scanner(System.in);
+		str = name.nextLine();
+        System.out.println("My name is: " + str);
+     return str;
+	}
+	
+	
+}
